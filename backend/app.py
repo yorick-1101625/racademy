60f7251a9c2c80f7a2335b4d9f70a85548b3c8ca
Marco-1028839 <marco@marcodoornbos.nl>
2025-04-16 11:54:17 +0200

    + Added setup for the flaskAlchemy + Added setup file db.py + Added a test table + Added a file to load test data and to create the new db.

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
+
 from backend.blueprints import register_blueprints
+from backend.database.db import init_db, db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HoiRuben.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+init_db(app)
+migrate = Migrate(app, db)
 CORS(app)
 register_blueprints(app)
 
