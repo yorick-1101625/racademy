082447ca2b602e4a727ef4bb705789403d8046e2
Ruben <rubenvanwoerkom@gmail.com>
2025-04-20 10:44:55 +0200

    Fixed small issue

--- backend/blueprints/__init__.py
+++ backend/blueprints/__init__.py
@@ -1,5 +1,7 @@
 from backend.blueprints.post import api_post
+from backend.blueprints.user import api_user
+
 
 def register_blueprints(app):
     app.register_blueprint(api_post, url_prefix="/api/post")
-    app.register_blueprint(api_post, url_prefix="/api/user")
+    app.register_blueprint(api_user, url_prefix="/api/user")
