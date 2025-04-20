da0f813c66d4fe262391f20b1cdd2aaeacafb4ad
yorick-1101625 <1101625@hr.nl>
2025-04-20 15:36:57 +0200

    Fix calculateTimeFromToday.js

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,4 +1,4 @@
-import {FlatList, Image, SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 
 import PostList from "../features/feed/components/PostList";
@@ -9,8 +9,6 @@ function Home() {
     return (
         <SafeAreaView className="flex-1 justify-center items-center bg-neutral-100">
 
-            {/*<FlatList>*/}
-            {/*</FlatList>*/}
             <PostList />
 
         </SafeAreaView>
--- frontend/features/feed/utils/calculateTimeFromToday.js
+++ frontend/features/feed/utils/calculateTimeFromToday.js
@@ -2,31 +2,58 @@ export default function calculateTimeFromToday(datetime) {
     const currentDate = new Date();
     const targetDate = new Date(datetime);
 
-    const yearDifference = currentDate.getFullYear() - targetDate.getFullYear();
-    if (yearDifference > 0) {
-        return `${yearDifference} jaar geleden`;
+    const years = currentDate.getFullYear() - targetDate.getFullYear();
+    if (years > 0) {
+        return `${years} jaar geleden`;
     }
 
-    const monthDifference = currentDate.getMonth() - targetDate.getMonth();
-    if (monthDifference > 0) {
-        return `${monthDifference} maanden geleden`;
+    const months = currentDate.getMonth() - targetDate.getMonth();
+    if (months > 0) {
+        return (
+            months === 1
+                ? '1 maand geleden'
+                : `${months} maanden geleden`
+        );
     }
 
-    const dayDifference = currentDate.getDay() - targetDate.getDay();
-    if (dayDifference > 0) {
-        return `${dayDifference} dagen geleden`;
+    const difference = currentDate - targetDate;
+    const weeks = Math.floor(difference / 1000 / 60 / 60 / 24 / 7);
+    if (weeks > 0) {
+        return (
+            weeks === 1
+                ? '1 week geleden'
+                : `${weeks} weken geleden`
+        );
     }
 
-    const hourDifference = currentDate.getHours() - targetDate.getHours();
-    if (hourDifference > 0) {
-        return `${hourDifference} uur geleden`;
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24);
+    if (days > 0) {
+        return (
+            days === 1
+                ? '1 dag geleden'
+                : `${days} dagen geleden`
+        );
     }
 
-    const minuteDifference = currentDate.getMinutes() - targetDate.getMinutes();
-    if (minuteDifference > 0) {
-        return `${minuteDifference} minuten geleden`;
+    const hours = Math.floor(difference / 1000 / 60 / 60);
+    if (hours > 0) {
+        return `${hours} uur geleden`;
     }
 
-    const secondDifference = currentDate.getSeconds() - targetDate.getSeconds();
-    return `${secondDifference} seconden geleden`;
+    const minutes = Math.floor(difference / 1000 / 60) % 60;
+    if (minutes > 0) {
+        return (
+            minutes === 1
+                ? '1 minuut geleden'
+                : `${minutes} minuten geleden`
+        );
+    }
+
+    const seconds = Math.floor(difference / 1000) % 60;
+
+    return (
+        seconds === 1
+            ? '1 seconde geleden'
+            : `${seconds} seconden geleden`
+    );
 }
