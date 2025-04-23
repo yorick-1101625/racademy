9abdb04ebb22f3308d2a90018921b83193433e9f
Ruben <rubenvanwoerkom@gmail.com>
2025-04-23 18:34:25 +0200

    Updated login route

--- backend/blueprints/login.py
+++ backend/blueprints/login.py
@@ -1,33 +1,45 @@
 from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
 from werkzeug.exceptions import HTTPException
-from backend.services.service_post import PostService
-from backend.models.models import *
+from backend.services.service_user import UserService
 
 api_login = Blueprint('api_login', __name__)
 
 @api_login.route('/', methods=['GET', 'POST', 'OPTIONS'])
+@cross_origin()
 def login():
-    if request.method == 'OPTIONS':
-        response = jsonify({'status': 'success',})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-        response.headers.add("Access-Control-Allow-Methods", "POST")
-        return response, 200
+    try:
+        data = request.get_json()
 
-    data = request.get_json(force=True)
-    print(data)
-    if check_login(data):
-        return jsonify({'status': 'success'}), 200
-    else:
-        return jsonify({'status': 'fail'}), 401
+        response = UserService.login_user(
+            data.get("email"),
+            data.get("password")
+        )
 
-
-def check_login(data):
-    user = User.query.filter_by(email=data['email']).first()
-    if user is None:
-        return False
-    else:
-        if user.password == data['password']:
-            return True
+        if response:
+            return jsonify({
+                "success": True,
+                "message": "Login succeeded."
+            }), 200
         else:
-            return False
+            return jsonify({
+                "success": False,
+                "message": "Login failed."
+            }), 401
+
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        print(f"[login] Unexpected error: {e}")
+        return jsonify({
+            "success": False,
+            "message": "An unexpected error occurred."
+        }), 500
+
+# TODO: Find better implementation for cors?
+# if request.method == 'OPTIONS':
+#     response = jsonify({'status': 'success'})
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+#     response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+#     return response, 200
