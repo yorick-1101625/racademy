cbd17fe83a295799d6ad7915a9d3e038b19f1203
yorick-1101625 <1101625@hr.nl>
2025-05-11 21:02:18 +0200

    Fetch user when page is reloaded, Redirect if user is not logged in

---
+++ frontend/components/UserOnly.jsx
@@ -0,0 +1,27 @@
+import useUser from "@/hooks/useUser";
+import {useRouter} from "expo-router";
+import {useEffect} from "react";
+import {ActivityIndicator} from "react-native";
+
+function UserOnly({ children }) {
+
+    // Redirect if user is not logged in
+
+    const { user, authChecked } = useUser();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (authChecked && user === null) {
+            router.replace('/');
+        }
+        console.log(authChecked, user)
+    }, [user, authChecked]);
+
+    if (!authChecked || !user) {
+        return <ActivityIndicator />
+    }
+
+    return children;
+}
+
+export default UserOnly;
