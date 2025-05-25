380d11388e1feb92f1f09a80fecc672663d604e3
kevin-1111102 <97698002+Kevin-1111102@users.noreply.github.com>
2025-05-25 17:45:46 +0200

    changed app.py to fix the issue with  "GET /static/user_images/profile_pictures/marco.webp HTTP/1.1" 404 -, the reason was that there were some context missing in app.py

--- backend/app.py
+++ backend/app.py
@@ -8,21 +8,27 @@ from flask_jwt_extended import JWTManager
 from backend.blueprints import register_blueprints
 from backend.database.db import init_db, db
 
-app = Flask(__name__)
+
+ROOT_PATH = Path(__file__).parent.resolve()
+STATIC_PATH = ROOT_PATH.parent / 'static'
 
 
+app = Flask(
+    __name__,
+    static_folder=str(STATIC_PATH)
+)
+
+# Configuratie
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'key'
-app.config['ROOT_PATH'] = Path(__file__).parent.resolve()
-app.config['IMAGE_UPLOAD_FOLDER'] = app.config['ROOT_PATH'] / 'static' / 'user_images'
-
+app.config['ROOT_PATH'] = ROOT_PATH
+app.config['IMAGE_UPLOAD_FOLDER'] = STATIC_PATH / 'user_images'
 
 init_db(app)
 migrate = Migrate(app, db)
 CORS(app)
 jwt = JWTManager(app)
-
 
 register_blueprints(app)
 
