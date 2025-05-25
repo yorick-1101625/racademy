78e480001fcc319918ac63fe5a5f6a59180d3082
Ruben <rubenvanwoerkom@gmail.com>
2025-05-25 09:17:49 +0200

    Added source delete modal

--- frontend/features/feed/components/source/SourceDetails.jsx
+++ frontend/features/feed/components/source/SourceDetails.jsx
@@ -4,17 +4,20 @@ import calculateTimeFromToday from "@/features/feed/utils/calculateTimeFromToday
 import formatRating from "@/features/feed/utils/formatRating";
 import getRatingIcon from "@/features/feed/utils/getRatingIcon";
 import {Ionicons} from "@expo/vector-icons";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {UserContext} from "@/contexts/UserContext";
 import fatty from "@/utils/fatty";
 import {showError, showSuccess} from "@/utils/toast";
+import BottomModal from "@/components/BottomModal";
 
 function
 SourceDetails({createdAt, schoolSubject, subject, rating, isBookmarked, handleBookmark, sourceUserId, sourceId}) {
 
-    const { user } = useContext(UserContext);
-
+    const {user} = useContext(UserContext);
     const userId = user.id;
+
+    const [modalVisible, setModalVisible] = useState(false);
+
 
     function handleDelete() {
 
@@ -25,6 +28,8 @@ SourceDetails({createdAt, schoolSubject, subject, rating, isBookmarked, handleBo
                 } else if (!data.success) {
                     showError("Er ging iets mis")
                 }
+                setModalVisible(false);
+
             });
     }
 
@@ -47,7 +52,8 @@ SourceDetails({createdAt, schoolSubject, subject, rating, isBookmarked, handleBo
                         />
                     </Pressable>
                     {
-                        sourceUserId === userId ? <Ionicons name="trash-outline" size={19} color="gray" onPress={handleDelete}/> : null
+                        sourceUserId === userId ?
+                            <Ionicons name="trash-outline" size={19} color="gray" onPress={() => setModalVisible(true)}/> : null
                     }
                 </View>
                 {
@@ -58,6 +64,32 @@ SourceDetails({createdAt, schoolSubject, subject, rating, isBookmarked, handleBo
                     </View>
                 }
             </View>
+
+             <BottomModal state={[modalVisible, setModalVisible]}>
+                <View className="items-center mb-4">
+                    <Ionicons name="trash-bin" size={64} color="#EF4444"/>
+                </View>
+
+                <Text className="text-lg font-semibold mb-6 text-center text-black">
+                    Weet je zeker dat je deze bron wilt verwijderen?
+                </Text>
+
+                <View className="flex-row justify-between space-x-3">
+                    <Pressable
+                        onPress={() => setModalVisible(false)}
+                        className="flex-1 py-3 bg-gray-200 rounded-md"
+                    >
+                        <Text className="text-center text-black font-semibold">Annuleren</Text>
+                    </Pressable>
+
+                    <Pressable
+                        onPress={handleDelete}
+                        className="flex-1 py-3 bg-red-600 rounded-md"
+                    >
+                        <Text className="text-center text-white font-semibold">Verwijderen</Text>
+                    </Pressable>
+                </View>
+            </BottomModal>
         </>
     );
 }
