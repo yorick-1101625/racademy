163ace8dfaf08a3c1fa144023088374d79b8592a
Ruben <rubenvanwoerkom@gmail.com>
2025-05-28 16:42:07 +0200

    Added post details. Updated post delete.

---
+++ frontend/app/(usr)/posts/[id].jsx
@@ -0,0 +1,38 @@
+import {ActivityIndicator, View} from 'react-native';
+import {useLocalSearchParams, useNavigation} from "expo-router";
+import useFetch from "@/hooks/useFetch";
+import Error from "@/components/Error";
+import Post from "@/features/feed/components/post/Post";
+import {useLayoutEffect} from "react";
+
+
+function PostDetails() {
+    const {id} = useLocalSearchParams();
+
+    const {data: post, isPending, error} = useFetch(`/api/post/${id}`);
+
+    const navigation = useNavigation();
+
+    // Set tab title at the top of the screen
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerTitle: () => null,
+        });
+    }, [navigation]);
+
+    if (isPending) return (
+        <View className="flex-1 justify-center items-center">
+            <ActivityIndicator size="large" color="#3daad3"/>
+        </View>
+    );
+
+    if (error) return (
+        <Error/>
+    );
+
+    return (
+        <Post post={post}/>
+    );
+}
+
+export default PostDetails;
