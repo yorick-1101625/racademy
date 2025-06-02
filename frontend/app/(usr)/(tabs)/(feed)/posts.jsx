64f7596d993f5effa919fadcd68e3076c6242c12
Ruben <rubenvanwoerkom@gmail.com>
2025-06-02 18:42:46 +0200

    Added ScrollToTopButton

--- frontend/app/(usr)/(tabs)/(feed)/posts.jsx
+++ frontend/app/(usr)/(tabs)/(feed)/posts.jsx
@@ -1,12 +1,16 @@
-import {SafeAreaView} from 'react-native';
+import {Animated, SafeAreaView} from 'react-native';
 import InfiniteScrollList from "@/components/InfiniteScrollList";
 import Post from "@/features/feed/components/post/Post";
 import {router, useFocusEffect, useLocalSearchParams} from "expo-router";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
+import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 function Posts() {
     const {refresh: refreshParam} = useLocalSearchParams();
     const [refresh, setRefresh] = useState(0);
+
+    const flatListRef = useRef(null);
+    const scrollY = useRef(new Animated.Value(0)).current;
 
     useFocusEffect(
         useCallback(() => {
@@ -17,6 +21,12 @@ function Posts() {
         }, [refreshParam])
     );
 
+    const scrollToTop = () => {
+        if (flatListRef.current) {
+            flatListRef.current.scrollToOffset({offset: 0, animated: true});
+        }
+    };
+
     // useEffect(() => {
     //     console.log('refreshFlag changed:', refreshFlag);
     // }, [refreshFlag]);
@@ -25,13 +35,18 @@ function Posts() {
         <SafeAreaView className="flex-1">
 
             <InfiniteScrollList
+                listRef={flatListRef}
+                onScroll={Animated.event(
+                    [{nativeEvent: {contentOffset: {y: scrollY}}}],
+                    {useNativeDriver: false}
+                )}
                 refresh={refresh}
                 renderItem={({item}) => <Post post={item}/>}
                 url="/api/post"
                 params="sort=recent"
                 noResultsMessage="Geen posts gevonden..."
             />
-
+            <ScrollToTopButton onPress={scrollToTop} scrollY={scrollY}/>
         </SafeAreaView>
     );
 }
