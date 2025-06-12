cdc85cf44bbd2b386a43ebdf2f09a178758e9e99
yorick-1101625 <1101625@hr.nl>
2025-06-12 14:35:08 +0200

    Turn off debug in Flask, Freeze pip packages in requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -38,4 +38,4 @@ def user_lookup_callback(_jwt_header, jwt_data):
 register_blueprints(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
