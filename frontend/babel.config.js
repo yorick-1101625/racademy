1114c926aae2ae3622c4a93bace4d8e4452a753b
yorick-1101625 <1101625@hr.nl>
2026-02-06 13:54:29 +0100

    update frontend deps to prepare for web build

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -5,5 +5,9 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      'babel-preset-expo',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
